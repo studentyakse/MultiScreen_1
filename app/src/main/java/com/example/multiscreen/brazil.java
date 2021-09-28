4a6570e94eef845c92db81d7d9b28c37aab81616
test <test>
2021-09-28 14:18:40 +0900

    Initial commit

---
+++ app/src/main/java/com/example/multiscreen/brazil.java
@@ -0,0 +1,14 @@
+package com.example.multiscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class brazil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_brazil);
+    }
+}
